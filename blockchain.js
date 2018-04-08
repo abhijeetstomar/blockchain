77fd30844e9909bd5329eaae5d371e1904569731
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-04-09 01:13:30 +0530

    Added test example

--- blockchain.js
+++ blockchain.js
@@ -30,3 +30,36 @@ class Block {
         return SHA256(this.index + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+    // This constructor is responsible for initialising our blockchain
+    constructor() {
+        // array of blocks
+        this.chain = [this.createGenesisBlock()];
+    }
+        
+    // First block of a blockchain is called Genesis block and should be added manually
+    createGenesisBlock() {
+        return new Block(0, "01/01/2018", "Genesis Block", "0");
+    }
+
+    // returns the latest block in the chain
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    // in reality you can't add a new block so easily since there are numerous checks in place
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+// testing our blockchain
+let myCoin = new Blockchain();
+myCoin.addBlock(new Block(1, "08/04/2018", { amount: 4 }));
+myCoin.addBlock(new Block(2, "10/04/2018", { amount: 10 }));
+
+// print the blockchain and use 4 spaces to format it
+console.log(JSON.stringify(myCoin, null, 4));
